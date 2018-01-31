799420e184657700e9589f8392dec37adfa73038
gusti amanda <gustijayaamanda@gmail.com>
2018-01-31 11:59:34 +0700

    first commit of my colors collection

---
+++ color.js
@@ -0,0 +1,8 @@
+//my favourite colors 
+export const whiteSmoke = '#F5F5F5';
+export const lightGray = '#D3D3D3';
+export const mediumPurple =  '#9370DB';
+export const darkGray = '#A9A9A9';
+export const dimGray = '#696969';
+
+
