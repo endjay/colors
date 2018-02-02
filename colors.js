19af8558df1a579096259fe70b5bd5399383a6f5
gusti amanda <gustijayaamanda@gmail.com>
2018-02-02 11:24:30 +0700

    add pallete 2

--- colors.js
+++ colors.js
@@ -1,8 +1,16 @@
-//my favourite colors 
+//pallete 1 
 export const whiteSmoke = '#F5F5F5';
 export const lightGray = '#D3D3D3';
 export const mediumPurple =  '#9370DB';
 export const darkGray = '#A9A9A9';
 export const dimGray = '#696969';
 
+//pallete 2 
+export const white = '#FFFFFF';
+export const dimGray = '#696969';
+export const yellowGreen = '#9ACD32';
+export const darkGoldenRod = '#B8860b';
+export const darkSalmon = '#E9967A';
+
+ 
 
